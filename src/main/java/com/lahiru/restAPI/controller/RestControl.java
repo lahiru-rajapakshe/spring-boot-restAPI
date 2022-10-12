77871f7dc482433f28e047911e62c2e354472bfb
lara <lahirurajapakshe.stack@gmail.com>
2022-10-12 11:17:48 +0530

    Finalise RestModel in Model

--- src/main/java/com/lahiru/restAPI/controller/RestControl.java
+++ src/main/java/com/lahiru/restAPI/controller/RestControl.java
@@ -1,14 +1,20 @@
 package com.lahiru.restAPI.controller;
 
+import com.lahiru.restAPI.model.RestModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class RestControl {
 
-    @PostMapping("/add")
-    public void addOrUpdateRecord(){
+    @Autowired
+    private RestModel restModel;
 
+    @PostMapping("/add")
+    public RestModel addOrUpdateRecord(@RequestBody RestModel restModel){
+
+        return restModel.addBLMethod(restModel);
     }
     @GetMapping("/all")
     public void finAllRecord(){
---
+++ src/main/java/com/lahiru/restAPI/service/RestService.java
@@ -0,0 +1,11 @@
+package com.lahiru.restAPI.service;
+
+import com.lahiru.restAPI.model.RestModel;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestService {
+
+
+
+}
